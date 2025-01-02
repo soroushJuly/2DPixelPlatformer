4265d4cd2e0fc06d5980fe455350660fe735efac
Soroush <soroushdevelope@gmail.com>
2025-01-02 04:10:11 +0000

    Add some game objects to the game

---
+++ Assets/Scripts/EnemyStaticTrap.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EnemyStaticTrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            collision.GetComponent<Health>().TakeDamage(damage);
+    }
+
+}
